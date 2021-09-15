5404fd3b67eb7cdb6997cbf42c73389641ec7a28
kge123 <88997999+kge123@users.noreply.github.com>
2021-09-15 18:53:48 -0400

    Added Confirm Boxes for all characters

--- script.js
+++ script.js
@@ -9,13 +9,37 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 // Function generatePassword
-//Add 
+//Add lowercase, uppercase, numerals, and special character variables
 function generatePassword() {
+  var choices =[lowerCase, upperCase, numbers, specialCharacters]
   var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
   var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   var numbers = '0123456789'
-  var specialCharacter = ' !"#$%&()*+,-.:;<=>?@[]`_^{}|~'
+  var specialCharacters = ' !"#$%&()*+,-.:;<=>?@[]`_^{}|~'
+  // Confirm box for lowercase letters included or not
+  var lowercaseCharacter = confirm('Do you want lowercase letters included in your password? OK=Yes Cancel=No')
+  if (lowercaseCharacter){
+    choices += lowerCase;
+  }
+  // Confirm box for uppercase letters included or not
+  var uppercaseCharacter = confirm('Do you want uppercase letters included in your password? OK=Yes Cancel=No')
+  if (uppercaseCharacter){
+    choices += upperCase;
+  }
+  // Confirm box for numerals to be included or not 
+  var numberCharacter = confirm('Do you want numerals included in your password? OK=Yes Cancel=No')
+  if (numberCharacter){
+    choices += numbers;
+  }
+  // Confirm box for special characters included or not 
+  var specialCharacterscharacter = confirm('Do you want special characters included in your password? OK=Yes Cancel=No')
+  if (specialCharacterscharacter){
+    choices += specialCharacters;
+  }
+
+  
 }
 
 // Add event listener to generate button
